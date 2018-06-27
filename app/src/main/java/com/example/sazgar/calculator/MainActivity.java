5a63c20f869a1e550c9bc3d70edf0a02cb31ed8c
mostafahosseini <hosseini.2009@yahoo.com>
2018-06-27 21:29:06 +0430

    functionalize
    شد functional  تا حدی

--- app/src/main/java/com/example/sazgar/calculator/MainActivity.java
+++ app/src/main/java/com/example/sazgar/calculator/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.sazgar.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +15,99 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       
 
+
+        final TextView textView = (TextView) findViewById(R.id.text);
+
+        final Button button7 = (Button) findViewById(R.id.button7);
+
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                textView.setText(button7.getText());
+
+            }
+        });
+
+
+        final Button button8 = (Button) findViewById(R.id.button8);
+
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                textView.setText(button8.getText());
+
+            }
+        });
+        final Button button9 = (Button) findViewById(R.id.button9);
+
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                textView.setText(button9.getText());
+            }
+        });
+        final Button button6 = (Button) findViewById(R.id.button6);
+
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                textView.setText(button6.getText());
+            }
+        });
+        final Button button5 = (Button) findViewById(R.id.button5);
+
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                textView.setText(button5.getText());
+            }
+        });
+        final Button button4 = (Button) findViewById(R.id.button4);
+
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                textView.setText(button4.getText());
+            }
+        });
+        final Button button3 = (Button) findViewById(R.id.button3);
+
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                textView.setText(button3.getText());
+            }
+        });
+        final Button button2 = (Button) findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+
+            textView.setText(button2.getText());
         }
+    }
+
+    );
+    final Button button1 = (Button) findViewById(R.id.button1);
+
+
+    button1.setOnClickListener(new View.OnClickListener()
+
+    {
+        public void onClick (View v){
+
+        textView.setText(button1.getText());
+    }
+    }
+
+    );
+}
 }
